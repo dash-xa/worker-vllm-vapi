d4e734ab09d842ae6bcf842f1b02c888124cde97
Alex Shevchuk <shevrlex@gmail.com>
2024-07-04 23:11:00 -0700

    update

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -4,5 +4,7 @@ set -euxo pipefail
 pushd ..
 model_name="${1:-sasdkafjlk/NeverSleep-Noromaid-13b-v0.3}"
 image_name="${2:-shevrlex/scarlett:latest}"
+docker login
 docker build -t "$image_name" --build-arg MODEL_NAME="$model_name" .
+docker push "$image_name"
 popd
