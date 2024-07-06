b72f382e834e87705ebad795ab3becd320cc6877
Alex Shevchuk <shevrlex@gmail.com>
2024-07-05 23:46:09 -0700

    better logging

--- src/handler.py
+++ src/handler.py
@@ -10,6 +10,8 @@ async def handler(job):
     job_input = JobInput(job["input"])
     engine = OpenAIvLLMEngine if job_input.openai_route else vllm_engine
     print("job_input is:", job_input)
+    job_input.llm_input
+    print("job['input'] is:", job["input"])
     results_generator = engine.generate(job_input)
     async for batch in results_generator:
         yield batch
